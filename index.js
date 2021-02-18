7edfc77c82f43084f9d2f5e02eda003f92c62674
Nick McCollum <nmccollum81@gmail.com>
2021-02-17 18:23:52 -0800

    Send form data to backend

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Goodbye World!')
+})
+
+app.post('/locationData', (req, res) => {
+  console.log(req.body)
+  res.send('Success')
 })
 
 app.listen(port, () => {
