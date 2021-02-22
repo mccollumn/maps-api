d823eb6635889d603490d36677dfc5c51132673e
Nick McCollum <nmccollum81@gmail.com>
2021-02-21 17:57:02 -0800

    Fixed DB connection

--- db.js
+++ db.js
@@ -1,17 +1,36 @@
-require("dotenv").config();
 const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const USER = process.env.MONGO_USERNAME;
-const PASSWORD = encodeURI(process.env.MONGO_PASSWORD);
+const PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD);
 
 // Replace the following with your Atlas connection string                                                                                                                                        
-const url = `mongodb+srv://${USER}:${PASSWORD}@cluster0.tyggb.mongodb.net?retryWrites=true&w=majority`;
+const url = `mongodb+srv://${USER}:${PASSWORD}@cluster0.tyggb.mongodb.net/maps?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
+const client = new MongoClient(url);
+// const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const dbName = "maps";
+
 async function run() {
     console.log(url);
-    const client = await new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
     try {
         await client.connect();
         console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        const col = db.collection("locations");
+
+        let locDoc = {
+            name: "Test Location",
+            rating: 3,
+            comment: "Cool test, man",
+            lat: 45.426884908241355,
+            long: -122.52172814602221
+        }
+
+        const p = await col.insertOne(locDoc);
+        const myDoc = await col.findOne();
+        console.log(myDoc);
     } catch (err) {
         console.log(err.stack);
     }
