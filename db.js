53edaed9dd7dd0237e4327bc8be7606bc15eec63
Nick McCollum <nmccollum81@gmail.com>
2021-03-04 18:18:31 -0800

    Removed run function

--- db.js
+++ db.js
@@ -4,76 +4,46 @@ require("dotenv").config();
 const USER = process.env.MONGO_USERNAME;
 const PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD);
 
-// Replace the following with your Atlas connection string                                                                                                                                        
+// Replace the following with your Atlas connection string
 const url = `mongodb+srv://${USER}:${PASSWORD}@cluster0.tyggb.mongodb.net/maps?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
 const client = new MongoClient(url);
 
 const dbName = "maps";
 
-async function run() {
-    console.log(url);
-
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const db = client.db(dbName);
-        const col = db.collection("locations");
-
-        let locDoc = {
-            name: "Test Location",
-            rating: 3,
-            comment: "Cool test, man",
-            lat: 45.426884908241355,
-            long: -122.52172814602221
-        }
-
-        const p = await col.insertOne(locDoc);
-        const myDoc = await col.findOne();
-        console.log(myDoc);
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
-}
-
 async function insertOne(payload, collectionName) {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const db = client.db(dbName);
-        const col = db.collection(collectionName);
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
+    const col = db.collection(collectionName);
 
-        return await col.insertOne(payload);
-    } catch (err) {
-        console.log(err.stack);
-    }
-    finally {
-        await client.close();
-    }
+    return await col.insertOne(payload);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
 }
 
 async function retrieveAll(collectionName) {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
-        const db = client.db(dbName);
-        const col = db.collection(collectionName);
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
+    const db = client.db(dbName);
+    const col = db.collection(collectionName);
 
-        //return await col.find({});
-        const cursor = col.find({})
-        let results = []
-        for await (const doc of cursor) {
-            results.push(doc)
-        }
-        return results;
-    } catch (err) {
-        console.log(err.stack);
+    //return await col.find({});
+    const cursor = col.find({});
+    let results = [];
+    for await (const doc of cursor) {
+      results.push(doc);
     }
-    finally {
-        await client.close();
-    }
+    return results;
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
 }
 
-module.exports = { run, insertOne, retrieveAll };
+module.exports = { insertOne, retrieveAll };
--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const { run, insertOne, retrieveAll } = require('./db')
-const app = express()
-const port = 3001
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { insertOne, retrieveAll } = require("./db");
+const app = express();
+const port = 3001;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-  await run()
-  res.send('Goodbye World!')
-})
+app.get("/", async (req, res) => {
+  res.send("Goodbye World!");
+});
 
-app.post('/locationData', async (req, res) => {
-  const dbRes = await insertOne(req.body, "locations") || {}
-  console.log(req.body)
-  console.log("DB Response:", dbRes)
+app.post("/locationData", async (req, res) => {
+  const dbRes = (await insertOne(req.body, "locations")) || {};
+  console.log(req.body);
+  console.log("DB Response:", dbRes);
   res.send({
     insertedCount: dbRes.insertedCount,
-    result: dbRes.ops
-  })
-})
+    result: dbRes.ops,
+  });
+});
 
-app.get('/locationData', async (req, res) => {
-  const dbRes = await retrieveAll("locations")
-  console.log("DB Response:", dbRes)
-  res.send(
-    { data: dbRes }
-  )
-})
+app.get("/locationData", async (req, res) => {
+  const dbRes = await retrieveAll("locations");
+  console.log("DB Response:", dbRes);
+  res.send({ data: dbRes });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
