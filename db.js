866cc212d232d2d582c916016a240705636e7f40
Nick McCollum <nmccollum@frontier.com>
2021-03-24 18:25:19 -0700

    Updated insertOne() and retrieveAll()

--- db.js
+++ db.js
@@ -4,13 +4,20 @@ require("dotenv").config();
 const USER = process.env.MONGO_USERNAME;
 const PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD);
 
-// Replace the following with your Atlas connection string
-const url = `mongodb+srv://${USER}:${PASSWORD}@cluster0.tyggb.mongodb.net/maps?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true`;
-const client = new MongoClient(url);
-
 const dbName = "maps";
 
+function getClient() {
+  const url = `mongodb+srv://${USER}:${PASSWORD}@cluster0.tyggb.mongodb.net/maps?retryWrites=true&w=majority`;
+
+  return new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
 async function insertOne(payload, collectionName) {
+  const client = getClient();
+
   try {
     await client.connect();
     console.log("Connected correctly to server");
@@ -26,19 +33,16 @@ async function insertOne(payload, collectionName) {
 }
 
 async function retrieveAll(collectionName) {
+  const client = getClient();
+
   try {
     await client.connect();
     console.log("Connected correctly to server");
-    const db = client.db(dbName);
-    const col = db.collection(collectionName);
+    const db = await client.db(dbName);
+    const col = await db.collection(collectionName);
+    const cursor = await col.find({});
 
-    //return await col.find({});
-    const cursor = col.find({});
-    let results = [];
-    for await (const doc of cursor) {
-      results.push(doc);
-    }
-    return results;
+    return await cursor.toArray();
   } catch (err) {
     console.log(err.stack);
   } finally {
